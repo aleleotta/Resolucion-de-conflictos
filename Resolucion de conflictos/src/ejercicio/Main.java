83f90d8d14fb0041936efc8ca06aed53baa42079
aleleotta <aleleotta@106-02.info2>
2022-12-13 09:40:55 +0100

    Las dos versiones se han fusionado.

--- Resolucion de conflictos/src/ejercicio/Main.java
+++ Resolucion de conflictos/src/ejercicio/Main.java
@@ -3,6 +3,6 @@ package ejercicio;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Yo soy Alessandro Leotta.");
-		System.out.println("Hola soy Yerai Jiménez");
+		System.out.println("Hola soy Yeray Jiménez");
 	}
 }
